653da610bec8f8a833b2f4630ad88b1d0c7e9654
JurgenHahn <jurgenehahn@gmail.com>
2017-02-15 15:45:07 -0500

    assignment finished

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
+$(function(){
 
+  $('button').on('click', function(){
 
+  $('body').toggleClass('dark');
+
+    if ($('button').attr('class') === 'on'){
+      $('button').attr('class', 'off');
+      $('h1').text('Hey, who turned off the lights?');
+      // $('body').attr('class', 'dark');
+    }
+    else {
+      $('button').attr('class', 'on');
+      $('h1').text("It's so bright in here!");
+      // $('body').attr('class', 'light');
+
+    }
+
+  })
+
+})
